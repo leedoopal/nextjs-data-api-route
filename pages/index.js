deec97e7ad049c5e41d24a190267980e9b0a8485
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-07 22:35:27 +0900

    preparing the frontend form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,34 @@
-export default function Home() {
+import React, { useRef } from 'react';
+
+function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+  }
+
   return (
     <div>
       <h1>Home</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback" ref={feedbackInputRef}>
+            Your Feedback
+          </label>
+          <textarea id="feedback" rows="5" />
+        </div>
+        <button>Send Feedback</button>
+      </form>
     </div>
-  )
+  );
 }
+
+export default Home;
