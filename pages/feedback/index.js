cdc9cd7d31c631cbb86cff5e139ff26e92a9a738
leedoopal <39582981+leedoopal@users.noreply.github.com>
2021-07-12 22:22:37 +0900

    dynamic api routes 사용

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { buildFeedbackPath, extractFeedback } from '../api/feedback';
 
 function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
+
   const { feedbackItems } = props;
   return (
-    <ul>
-      {feedbackItems.map((item) => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}{' '}
+            <button
+              type="button"
+              onClick={loadFeedbackHandler.bind(null, item.id)}
+            >
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
